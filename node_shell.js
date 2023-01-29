725b2944155297f04b347012b550a7f2cc839916
ctrlmelive <118881107+ctrlmelive@users.noreply.github.com>
2023-01-29 09:39:02 +0000

    Update node_shell.js

--- node_shell.js
+++ node_shell.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'bc249f45-7f7e-4bd0-94a3-c499c1a28c79'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
